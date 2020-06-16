d158abb6eafee450c1aac457a2b671f6db5ad091
Leander van Grinsven <leander550@live.nl>
2020-06-16 10:04:49 +0200

    Moved the Splashactivity to the correct folder.

--- app/src/main/java/com/example/FiveM_Panel/ui/SplashActivity.kt
+++ app/src/main/java/com/example/FiveM_Panel/ui/SplashActivity.kt
@@ -1,10 +1,10 @@
-package com.example.FiveM_Panel
+package com.example.FiveM_Panel.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.example.FiveM_Panel.ui.MainActivity
+import com.example.FiveM_Panel.R
 
 class SplashActivity : AppCompatActivity() {
 
